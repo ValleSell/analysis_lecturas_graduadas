1714e1aa6e06794871ec30655672c55cd16552d8
Valle Sell <valle.sell@gmx.de>
2022-08-12 18:12:22 +0200

    Funktion für Hyponymensuche verbessert

--- crea_lista_woman_man_terms_EN_WordNet.py
+++ crea_lista_woman_man_terms_EN_WordNet.py
@@ -1,45 +1,43 @@
 import nltk
+nltk.download('omw-1.4')
 from nltk.corpus import wordnet as wn
 
 import os
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('--output_dir', required=True)
+#parser.add_argument('--output_dir', required=True)
 args = parser.parse_args()
 
-def busca_hipos (var):
-    #la función puede partir de una palabra o una lista (var), busca en WordNet
-    #y crea una lista que inlcuye la palabra o la lista de palabras inicial más todos
-    #los hipónimos de esta incluidos en WordNet
+def busca_hipos_lista (l, lexname=None):
+    result=[]
+    for e in l:
+        sys = wn.synsets(e)
+        for sy in sys:
+            hy = sy.hyponyms()
+            lista = [lemma.name() for synset in hy for lemma in synset.lemmas() if lexname == None or synset._lexname == lexname]
+            l=l+lista
+    [result.append(e) for e in l if e not in result]
+    return result
+
+
+def busca_hipos_pal (var, n=1, lexname= None):
+    #la función parte de una lista de palabras (var) y busca hipónimos en WordNet
+    #y crea una lista que incluye la palabra inicial más todos
+    #los hipónimos de esta incluidos y los hipónimos de los hipónimos en n niveles en WordNet
     if type(var)==str:
-        #en este caso la variable es una palabra (un string, str)
-        wns=wn.synsets(var)
-        gen_words=[lemma.name() for synset in wns for lemma in synset.lemmas() if synset._lexname=="noun.person"]
-        terms=[]
-        # borra nombres_signif , ' y duplicados
-        [terms.append((e.replace("nombres_signif", " ")).replace("`", "")) for e in gen_words if e not in terms]
-        return terms
-    elif type (var)==list:
-        #en este caso la variable es una lista (list)
-        for e in var:
-            sys=wn.synsets(e)
-            for sy in sys:
-                if sy._lexname=="noun.person":
-                    hy=sy.hyponyms()
-                    lista=[lemma.name() for synset in hy for lemma in synset.lemmas()]
-                    var=var+lista
-    terms=[]
-    [terms.append((e.replace("nombres_signif", " ")).replace("`", "")) for e in var if e not in terms]
-    return terms
+        var =[var]
+    result_final=var
+    result=var
+    for z in range (n-1):
+        result=busca_hipos_lista(result,lexname)
+        [result_final.append(e) for e in result if e not in result_final]
+
+    return result_final
 
 
-#tb=término a buscar
-tb="woman"
-#n=número de iteraciones
-n=4
-women_words= busca_hipos (tb)
-for e in range (n):
-    women_words= busca_hipos (women_words)
+#crea un listado con hipónimos de "woman" de cuatro niveles de profundidad
+#incluyendo únicamente sustantivos para designar personas
+women_words= busca_hipos_pal ("woman", 4, "noun.person")
 
 
 #escribe un archivo con los resultados
@@ -49,13 +47,10 @@ with open(os.path.join(args.output_dir, "gender_terms.csv"), "a", encoding="utf-
         f.write(e + ",women,gender" + '\n')
 
 #repetir para hombres
-tb="man"
-n=4
-men_words= busca_hipos (tb)
-for e in range (n):
-    men_words= busca_hipos (men_words)
+men_words= busca_hipos_pal ("man", 4, "noun.person")
 
-os.makedirs(args.output_dir, exist_ok=True)
+
+#os.makedirs(args.output_dir, exist_ok=True)
 with open(os.path.join(args.output_dir, "gender_terms.csv"), "a", encoding="utf-8") as f:
     for e in men_words:
         f.write(e + ",men,gender" + '\n')
